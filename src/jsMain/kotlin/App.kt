fbf0f7f63d825f852c95f992bd450eca77ea9a29
Paul Martin <cogranpm@gmail.com>
2020-12-10 10:32:06 -0600

    following along with hands on

---
+++ src/jsMain/kotlin/App.kt
@@ -0,0 +1,29 @@
+import react.*
+import react.dom.*
+import kotlinext.js.*
+import kotlinx.html.js.*
+import kotlinx.coroutines.*
+
+private val scope = MainScope()
+
+val App = functionalComponent<RProps> { _ ->
+    val (shoppingList, setShoppingList) = useState(emptyList<ShoppingListItem>())
+
+    useEffect (dependencies = listOf()) {
+        scope.launch {
+            setShoppingList(getShoppingList())
+        }
+    }
+
+    h1 {
+        +"Full-Stack Shopping List"
+    }
+    ul {
+        shoppingList.sortedByDescending(ShoppingListItem::priority).forEach { item ->
+            li {
+                key = item.toString()
+                +"[${item.priority}] ${item.desc}"
+            }
+        }
+    }
+}
